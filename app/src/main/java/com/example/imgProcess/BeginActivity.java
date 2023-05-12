c246ab10808379576dfdb82e8334bcba07f2353f
u4E0A <61388807+u4E0A@users.noreply.github.com>
2023-05-12 22:10:10 +0800

    绑定了begin活动的按钮

--- app/src/main/java/com/example/imgProcess/BeginActivity.java
+++ app/src/main/java/com/example/imgProcess/BeginActivity.java
@@ -3,6 +3,8 @@ package com.example.imgProcess;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.imgprocess.R;
 
@@ -12,5 +14,15 @@ public class BeginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_begin);
+
+        Button openFromFile = findViewById(R.id.button_openFromFile);
+        openFromFile.setOnClickListener(v -> {
+            Toast.makeText(this,"good",Toast.LENGTH_SHORT).show();
+        });
+
+        Button takePhoto = findViewById(R.id.button_takePhoto);
+        takePhoto.setOnClickListener(v -> {
+            Toast.makeText(this,"good",Toast.LENGTH_SHORT).show();
+        });
     }
 }
--- app/src/main/java/com/example/imgProcess/MainActivity.java
+++ app/src/main/java/com/example/imgProcess/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.imgProcess;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import androidx.appcompat.app.AppCompatActivity;
 import com.example.imgprocess.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
