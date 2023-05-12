a7077464a1de7ca54ab0d92934a22a0ec527993c
u4E0A <61388807+u4E0A@users.noreply.github.com>
2023-05-12 21:22:25 +0800

    add views and activities

---
+++ app/src/main/java/com/example/imgProcess/BeginActivity.java
@@ -0,0 +1,16 @@
+package com.example.imgProcess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.imgprocess.R;
+
+public class BeginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_begin);
+    }
+}
